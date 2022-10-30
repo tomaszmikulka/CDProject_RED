4d9d0017be9e4dc04911bd580ff6e30310a1d371
Tomasz <tomaszmikulka@gmail.com>
2022-10-30 13:03:20 +0100

    Pierwsze zadanie gotowe !

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero bohater1 = new Hero("Tomek","Zabójca",1,20,true);
+        Hero bohater2 = new Hero("imie","klasa",true);
+    }
+}
